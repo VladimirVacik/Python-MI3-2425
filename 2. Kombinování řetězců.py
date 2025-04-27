446f13b49c4cf9c73a01a730692eb94df95d473b
VladimirVacik <vacik@souepl.cz>
2025-04-27 16:24:36 +0200

    úprava příkladů

--- 2. Kombinování řetězců.py
+++ 2. Kombinování řetězců.py
@@ -16,10 +16,10 @@ print ("Ahoj \"Karle\"!") #ukázka uvozovek
 print ("C:\\Github\\Python") #ukázka zpětného lomítka
 
 #Formátování výstupu pomocí řídících znaků
-print("Generating invoices for customer \"Contoso Corp\" ... \n");
-print("Invoice: 1021\t\tComplete!");
-print("Invoice: 1022\t\tComplete!");
-print("\nOutput Directory:\t");
+print("Analýzy prospěchu žáků ... \n");
+print("Třída: IT1A\t\tHotovo");
+print("Třída: IT2B\t\tHotovo");
+print("\nAnalýza je uložena ve složce:\t");
 
 #Doslovný řetězec
 #Doslovný řetězec zachovává všechny mezery a znaky bez nutnosti použít řídicí znak (zpětné lomítko).
@@ -32,39 +32,4 @@ print(r"    C:\souerce\repos\
 #Například: ©, ™, ®, €, £, ¥, ¢, §, ¶, ÷, ±, µ, ∞, ∑, ∏, ∫, ∆, √
 #Unicode znaky začínají znakem "\u" a za ním následuje čtyřmístné hexadecimální číslo.
 
-print("\u3053\u3093\u306b\u3061\u306f World") #výstup Kon'nichiwa World
-# Řídící sekvence znaků pomocí znaku \ se používají pro speciální znaky, které nemůžeme napsat přímo do textu, nebo pro úpravu textu
-# například:
-# \ - zpětné lomítko, které říká, že následující znak má speciální význam, dá se použít na konci řádku pro pokračování na dalším řádku
-# \n - nový řádek
-# \t - tabulátor
-# \\ - zpětné lomítko
-# \' - apostrof
-# \" - uvozovky
-# \r - návrat na začátek řádku
-# \( - levá závorka
-
-print ("Hello World") #ukázka výstupu
-print ("Hello\nWorld") #ukázka nového řádku
-print ("Hello\tWorld") #ukázka tabulátoru
-print ("Hello \"World\"!") #ukázka uvozovek
-print ("C:\\source\\repos") #ukázka zpětného lomítka
-
-#Formátování výstupu pomocí řídících znaků
-print("Generating invoices for customer \"Contoso Corp\" ... \n");
-print("Invoice: 1021\t\tComplete!");
-print("Invoice: 1022\t\tComplete!");
-print("\nOutput Directory:\t");
-
-#Doslovný řetězec
-#Doslovný řetězec zachovává všechny mezery a znaky bez nutnosti použít řídicí znak (zpětné lomítko).
-#Pro použití doslovného řetězce použijeme znak "r" před uvozovkami.
-print(r"    C:\souerce\repos\
-      (sem patří tvůj kod)")
-
-#Řídicí znaky Unicode
-#Pomocí Unicode můžeme použít znaky, které nejsou na klávesnici.
-#Například: ©, ™, ®, €, £, ¥, ¢, §, ¶, ÷, ±, µ, ∞, ∑, ∏, ∫, ∆, √
-#Unicode znaky začínají znakem "\u" a za ním následuje čtyřmístné hexadecimální číslo.
-
-print("\u3053\u3093\u306b\u3061\u306f World") #výstup Kon'nichiwa World
+print("Tři japonské opice:\nMizaru - \u307F\u3056\u308B\nKikazaru - \u304D\u304B\u3056\u308B\nIwazaru - \u3044\u308F\u3056\u308B") #výstup Japonské opice: Mizaru (nevidím zlo), Kikazaru (vidím zlo), Iwanaga (zlo)
