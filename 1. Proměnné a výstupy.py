ce4e89f1faae0b19159474712a406b160e2a3f80
VladimirVacik <vacik@souepl.cz>
2025-04-27 12:12:41 +0200

    úprava souboru

--- 1. Proměnné a výstupy.py
+++ 1. Proměnné a výstupy.py
@@ -18,6 +18,7 @@ print (jmeno) #výstup
 print (bmi) #výstup
 
 #staticé proměnné pomocí hints
+#s rozšířením type hints můžeme definovat typ proměnné a díky pylance můžeme kontrolovat nechtěné přetypování proměnných
 vek1: int = 25 #proměnná typu int - celé číslo
 vyska1: float = 175.5 #proměnná typu float - reálné číslo
 vaha1: float = 70.5 #proměnná typu float - reálné číslo
@@ -25,4 +26,4 @@ nadvaha1: bool = True #proměnná typu bool - logická hodnota místo true/false
 jmeno1: str = "Petr" #proměnná typu str - řetězec
 bmi1: float #proměnná typu float - reálné číslo jedná se o deklaraci bez inicializace
 bmi1 = vaha1 / ((vyska1 / 100) ** 2) #výpočet BMI
-print (bmi1) #výstup 
+print (bmi1) #výstup
