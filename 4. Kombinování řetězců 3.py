829d2ad7e5e3a78920cdc075443ca084a811d858
VladimirVacik <vacik@souepl.cz>
2025-04-27 20:22:11 +0200

    interpolace

--- 4. Kombinování řetězců 3.py
+++ 4. Kombinování řetězců 3.py
@@ -1,2 +1,15 @@
 # Kombinování řetězců pomocí interpolace
 # Interpolace je způsob, jakým můžeme do řetězce vložit hodnoty proměnných.
+# Interpolace se provádí pomocí znaku f a složených závorek {}. Do složených závorek se vloží název proměnné, kterou chceme vložit do řetězce.
+# Interpolace se používá v Pythonu 3.6 a vyšších verzích.
+# Interpolace se provádí pomocí f-strings, což je způsob, jakým můžeme do řetězce vložit hodnoty proměnných.
+
+jmeno = "Karel"
+prijmeni = "Novák"
+zprava = f"Jeho příjmení je {prijmeni}."
+print(zprava) #výstup celého jména
+
+# Pouužití interpolace řetězců s více proměnnými
+celeJmeno = f"{jmeno} {prijmeni}"
+print(celeJmeno) #výstup celého jména
+print(f"Celé jméno je {jmeno} {prijmeni}.") #výstup celého jména
