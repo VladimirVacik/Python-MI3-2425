4563e7b4f2b4273e24def45cddc8f00bb603d3b9
VladimirVacik <vacik@souepl.cz>
2025-04-27 20:51:55 +0200

    čísla

---
+++ 5. Výstup řetězce a čísla.py
@@ -0,0 +1,16 @@
+# Přidání dvou číselných proměnných
+prvniCislo = 20
+druheCislo = 10
+print(prvniCislo + druheCislo) #výstup součtu čísel
+
+# Kombinace čísel a řetězců
+print("Rozdíl čísel je: ", prvniCislo - druheCislo) #výstup rozdílu čísel
+# při zřetězení se musí čísla převést na řetězce, tomu se říká přetypování
+print("Prvni číslo je " + str(prvniCislo) + " a druhe číslo je " + str(druheCislo)) #výstup čísel pomocí zřetězení
+
+#Složitější příklad
+print(f"{prvniCislo} + {druheCislo} = {prvniCislo + druheCislo}") #výstup součtu čísel pomocí interpolace
+
+
+
+
